05effb7c9b7ea58e05a576911ab6487d209796b2
citypathologylaboratory-svg <citypathologylaboratory@gmail.com>
2025-10-17 09:51:52 +0530

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    domains: ['images.unsplash.com'],
+    unoptimized: false,
+  },
+}
+
+module.exports = nextConfig
